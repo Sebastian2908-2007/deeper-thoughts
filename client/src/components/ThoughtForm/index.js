764aa602f058ce667d6310a2433d662d97e9fb41
Sebabin <sebastiancrespin279@gmail.com>
2022-04-21 00:26:00 -0600

    have thought  form set up and thought being submitted to the data base as well as properly updating on the page

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,36 @@
 import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_THOUGHT } from "../../utils/mutations";
+import { QUERY_THOUGHTS, QUERY_ME } from "../../utils/queries";
 
 const ThoughtForm = () => {
     const [thoughtText, setText]= useState('');
     const [characterCount, setCharacterCount] = useState(0);
+    // declare our mutation function
+    const [addThought, {error}] = useMutation(ADD_THOUGHT, {
+        // below addThought represents the new thought that was just created
+        update(cache, { data: { addThought } }) {
+            try { 
+            // read whats currently in the cache could potentially not exist yet, so wrap in a try...catch
+            const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+            // preappend the newest thought to the front of the array
+            cache.writeQuery({
+                query: QUERY_THOUGHTS,
+                data: { thoughts: [addThought, ...thoughts] }
+            });
+        } catch (e) {
+            console.error(e);
+        }
+
+        // update me objects cache appending new thought to the end of the array the varname to be in {} should be the same as the type the query returns in the typeDefs
+        const { me } = cache.readQuery({ query: QUERY_ME });
+        cache.writeQuery({
+            query: QUERY_ME,
+            data:{ me: { ...me, thoughts: [...me.thoughts, addThought]}}
+        });
+        }
+    });
 
     // function for setting values as the user types
     const handleChange = event => {
@@ -14,14 +42,24 @@ const ThoughtForm = () => {
     // submit handler
     const handleFormSubmit = async event => {
         event.preventDefault();
-        setText('');
-        setCharacterCount(0);
+      
+        try {
+            // add thought to data base
+            await addThought({
+                variables: { thoughtText }
+            });
+            // clear form value
+            setText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
     };
 
     return (
         <div>
-            <p className={`m-0 ${characterCount === 280 ? 'text-error': ''}`}>
-                Character Count: {characterCount}/280
+            <p className={`m-0 ${characterCount === 280 || error ? 'text-error': ''}`}>
+                Character Count: {characterCount}/280 {error && <span className="m1-2">Something went wrong...</span>}    
             </p>
             <form className="flex-row justify-center justify-space-between-md align-stretch" onSubmit={handleFormSubmit}>
                 <textarea
